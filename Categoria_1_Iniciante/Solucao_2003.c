b8649ae192b891ffde86d6168e5f7d391e2ff014
Paulo Henrique Carvalho <paulohcarvalho9@gmail.com>
2016-08-08 18:17:53 -0300

    Add solução 2003

---
+++ Categoria_1_Iniciante/Solucao_2003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int hora, minuto, atraso;
+	while (scanf("%d:%d", &hora, &minuto) != EOF) {
+		atraso = 0;
+		if (hora >= 7) {
+      atraso = 60*(hora - 7) + minuto;
+    }
+		printf("Atraso maximo: %d\n", atraso);
+	}
+	return 0;
+}
